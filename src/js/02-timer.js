d77c641db0bdfcdc17a08484e637732c82774896
AlexSanako <116287214+AlexSanako@users.noreply.github.com>
2023-05-02 12:03:59 +0300

    допрацювання

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,6 +9,40 @@ const minutesValue = document.querySelector('[data-minutes]');
 const secondsValue = document.querySelector('[data-seconds]');
 
 let intervalId;
+let selectedDate;
+
+function updateTimer() {
+  const now = new Date().getTime();
+  const distance = selectedDate - now;
+  if (distance < 0) {
+    clearInterval(intervalId);
+    daysValue.textContent = '00';
+    hoursValue.textContent = '00';
+    minutesValue.textContent = '00';
+    secondsValue.textContent = '00';
+    return;
+  }
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    .toString()
+    .padStart(2, '0');
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  )
+    .toString()
+    .padStart(2, '0');
+  const minutes = Math.floor(
+    (distance % (1000 * 60 * 60)) / (1000 * 60)
+  )
+    .toString()
+    .padStart(2, '0');
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+    .toString()
+    .padStart(2, '0');
+  daysValue.textContent = days;
+  hoursValue.textContent = hours;
+  minutesValue.textContent = minutes;
+  secondsValue.textContent = seconds;
+}
 
 const options = {
   enableTime: true,
@@ -16,50 +50,82 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    if (selectedDate < new Date()) {
+    const newSelectedDate = selectedDates[0];
+    if (newSelectedDate < new Date()) {
       window.alert('Please choose a date in the future');
       startButton.disabled = true;
-    } else {
-      startButton.disabled = false;
-      startButton.addEventListener('click', () => {
-        clearInterval(intervalId);
-        const countdownDate = selectedDate.getTime();
-        intervalId = setInterval(() => {
-          const now = new Date().getTime();
-          const distance = countdownDate - now;
-          if (distance < 0) {
-            clearInterval(intervalId);
-            daysValue.textContent = '00';
-            hoursValue.textContent = '00';
-            minutesValue.textContent = '00';
-            secondsValue.textContent = '00';
-            return;
-          }
-          const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-            .toString()
-            .padStart(2, '0');
-          const hours = Math.floor(
-            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-          )
-            .toString()
-            .padStart(2, '0');
-          const minutes = Math.floor(
-            (distance % (1000 * 60 * 60)) / (1000 * 60)
-          )
-            .toString()
-            .padStart(2, '0');
-          const seconds = Math.floor((distance % (1000 * 60)) / 1000)
-            .toString()
-            .padStart(2, '0');
-          daysValue.textContent = days;
-          hoursValue.textContent = hours;
-          minutesValue.textContent = minutes;
-          secondsValue.textContent = seconds;
-        }, 1000);
-      });
-    }
+      return;
+    } 
+    startButton.disabled = false;
+    selectedDate = newSelectedDate.getTime();
+    clearInterval(intervalId);
+    intervalId = setInterval(updateTimer, 1000);
   },
 };
 
 flatpickr(datetimePicker, options);
+
+
+
+// const datetimePicker = document.getElementById('datetime-picker');
+// const startButton = document.querySelector('[data-start]');
+// const daysValue = document.querySelector('[data-days]');
+// const hoursValue = document.querySelector('[data-hours]');
+// const minutesValue = document.querySelector('[data-minutes]');
+// const secondsValue = document.querySelector('[data-seconds]');
+
+// let intervalId;
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     const selectedDate = selectedDates[0];
+//     if (selectedDate < new Date()) {
+//       window.alert('Please choose a date in the future');
+//       startButton.disabled = true;
+//     } else {
+//       startButton.disabled = false;
+//       startButton.addEventListener('click', () => {
+//         clearInterval(intervalId);
+//         const countdownDate = selectedDate.getTime();
+//         intervalId = setInterval(() => {
+//           const now = new Date().getTime();
+//           const distance = countdownDate - now;
+//           if (distance < 0) {
+//             clearInterval(intervalId);
+//             daysValue.textContent = '00';
+//             hoursValue.textContent = '00';
+//             minutesValue.textContent = '00';
+//             secondsValue.textContent = '00';
+//             return;
+//           }
+//           const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+//             .toString()
+//             .padStart(2, '0');
+//           const hours = Math.floor(
+//             (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//           )
+//             .toString()
+//             .padStart(2, '0');
+//           const minutes = Math.floor(
+//             (distance % (1000 * 60 * 60)) / (1000 * 60)
+//           )
+//             .toString()
+//             .padStart(2, '0');
+//           const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+//             .toString()
+//             .padStart(2, '0');
+//           daysValue.textContent = days;
+//           hoursValue.textContent = hours;
+//           minutesValue.textContent = minutes;
+//           secondsValue.textContent = seconds;
+//         }, 1000);
+//       });
+//     }
+//   },
+// };
+
+// flatpickr(datetimePicker, options);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,3 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
 function createPromise(position, delay, shouldResolve) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -38,14 +29,70 @@ form.addEventListener("submit", (event) => {
     promises.push(promise);
   }
 
-  Promise.allSettled(promises).then((results) => {
-    results.forEach((result) => {
-      const { position, delay } = result.value || result.reason;
-      if (result.status === "fulfilled") {
+  promises.forEach((promise) => {
+    promise
+      .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      } else {
+      })
+      .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      }
-    });
+      });
   });
 });
+
+
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+
+
+// function createPromise(position, delay, shouldResolve) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// const form = document.querySelector(".form");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const delay = parseInt(form.elements.delay.value);
+//   const step = parseInt(form.elements.step.value);
+//   const amount = parseInt(form.elements.amount.value);
+
+//   const promises = [];
+
+//   for (let i = 0; i < amount; i++) {
+//     const position = i + 1;
+//     const promiseDelay = delay + i * step;
+//     const shouldResolve = Math.random() > 0.3;
+//     const promise = createPromise(position, promiseDelay, shouldResolve);
+//     promises.push(promise);
+//   }
+
+//   Promise.allSettled(promises).then((results) => {
+//     results.forEach((result) => {
+//       const { position, delay } = result.value || result.reason;
+//       if (result.status === "fulfilled") {
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       } else {
+//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//       }
+//     });
+//   });
+// });
